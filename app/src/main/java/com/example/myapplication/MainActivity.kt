791575d975ddf96af2a30e177e9e140ef02c0942
Jainsaheb__ <phagunjain@protonmail.com>
2021-08-26 22:14:30 +0530

    wrote activity code

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
 
@@ -14,8 +15,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
 
+    fun ShowContact(view:View){
         show = findViewById(R.id.show)
         showDetail = findViewById(R.id.showDetail)
+
+        var detail = "example.gmail.com"
+        showDetail.setText(detail)
     }
 }
